8e6ef83bf2eb909d77067f6d55fc6ebf9272d43a
Petru-Nicu Dumitrache <berserk.ro@gmail.com>
2020-03-28 14:28:26 +0100

    Finished p88

--- problems/p88.h
+++ problems/p88.h
@@ -12,11 +12,24 @@ namespace p88
         {
             // merge in place in nums1, working backwards
 
-            size_t current_merged = nums1.size() - 1;
-            size_t current_1 = static_cast<size_t>(m) - 1;
-            size_t current_2 = static_cast<size_t>(n) - 1;
-            while (current_merged >= 0)
+            int current_merged = m + n - 1;
+            int current_1 = m - 1;
+            int current_2 = n - 1;
+            while (true)
             {
+                // if stuff from nums1 is finished, just copy what's not merged yet from nums2 into the start of nums1
+                if (current_1 == -1)
+                {
+                    std::copy(nums2.begin(), nums2.begin() + current_2 + 1, nums1.begin());
+                    break;
+                }
+
+                // if stuff from nums2 is finished, just exit
+                if (current_2 == -1)
+                {
+                    break;
+                }
+
                 if (nums1[current_1] < nums2[current_2])
                 {
                     nums1[current_merged] = nums2[current_2];
--- tests/test.cpp
+++ tests/test.cpp
@@ -2,8 +2,26 @@
 
 #include "../problems/p88.h"
 
-TEST(TestCaseName, TestName)
+TEST(P88, T0)
 {
-    EXPECT_EQ(1, 1);
-    EXPECT_TRUE(true);
+    std::vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+    std::vector<int> nums2 = {2, 5, 6};
+
+    p88::Solution s;
+    s.merge(nums1, 3, nums2, 3);
+
+    std::vector<int> expected = {1, 2, 2, 3, 5, 6};
+    EXPECT_EQ(nums1, expected);
+}
+
+TEST(P88, T1)
+{
+    std::vector<int> nums1 = {0};
+    std::vector<int> nums2 = {1};
+
+    p88::Solution s;
+    s.merge(nums1, 0, nums2, 1);
+
+    std::vector<int> expected = {1};
+    EXPECT_EQ(nums1, expected);
 }
