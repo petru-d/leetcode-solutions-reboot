45725b0dc68e2a8dbd5c78f37b3106fb3665cb1d
Petru-Nicu Dumitrache <berserk.ro@gmail.com>
2020-04-21 01:28:50 +0200

    re-added utility function lost in the previous commit mess

--- problems/util_binary_tree.h
+++ problems/util_binary_tree.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <optional>
+#include <vector>
+
 struct TreeNode
 {
     int val;
@@ -45,4 +48,47 @@ inline TreeNode* make_binary_tree(int l_val, int root_val, int r_val)
     root->left = left;
     root->right = right;
     return root;
+}
+
+inline std::vector<std::optional<int>> to_vector_levelwise_full(TreeNode* root)
+{
+    if (root == nullptr)
+        return {};
+
+    std::vector<std::vector<TreeNode*>> rows = {{root}};
+
+    while (true)
+    {
+        const auto& last_row = rows.back();
+
+        bool found_non_null = false;
+        std::vector<TreeNode*> new_row;
+        for (auto* node : last_row)
+        {
+            if (nullptr == node)
+            {
+                new_row.push_back(nullptr);
+                new_row.push_back(nullptr);
+            }
+            else
+            {
+                if (node->left != nullptr || node->right != nullptr)
+                    found_non_null = true;
+                new_row.push_back(node->left);
+                new_row.push_back(node->right);
+            }
+        }
+
+        if (!found_non_null)
+            break;
+
+        rows.emplace_back(std::move(new_row));
+    }
+
+    std::vector<std::optional<int>> result;
+    for (const auto& row : rows)
+        for (const auto* node : row)
+            result.push_back(node != nullptr ? node->val : std::optional<int>());
+
+    return result;
 }
