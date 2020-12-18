b75d4c955c18b34c2200162bd595d7928cb940f6
Petru-Nicu Dumitrache <berserk.ro@gmail.com>
2020-12-18 22:14:15 +0100

    p382 solution

--- problems/p382.h
+++ problems/p382.h
@@ -1,8 +1,48 @@
 #pragma once
+#include "util_linked_list.h"
+#include <random>
 
 namespace p382
 {
     class Solution
     {
+      public:
+        /** @param head The linked list's head.
+            Note that the head is guaranteed to be not null, so it contains at least one node. */
+        Solution(ListNode* head)
+        {
+            _head = head;
+
+            std::random_device rd;
+            _gen = std::mt19937(rd());
+
+            int len = 0;
+            while (head)
+            {
+                ++len;
+                head = head->next;
+            }
+
+            _distrib = std::uniform_int_distribution<>(0, len - 1);
+        }
+
+        /** Returns a random node's value. */
+        int getRandom()
+        {
+            int advance = _distrib(_gen);
+
+            auto* curr = _head;
+            for (int i = 0; i < advance; ++i)
+            {
+                curr = curr->next;
+            }
+
+            return curr->val;
+        }
+
+      private:
+        ListNode* _head;
+        std::mt19937 _gen;
+        std::uniform_int_distribution<> _distrib;
     };
 }
--- tests/p382.cpp
+++ tests/p382.cpp
@@ -4,5 +4,9 @@
 
 TEST(p382, t0)
 {
-    [[maybe_unused]] p382::Solution s;
+    p382::Solution s(make_list({1,2,3}));
+
+    auto res = s.getRandom();
+
+    EXPECT_TRUE(1 <= res && res <= 3);
 }
