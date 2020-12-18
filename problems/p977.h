93b39a5bd6f66cae00852a5231f445d61ab15087
Petru-Nicu Dumitrache <berserk.ro@gmail.com>
2020-12-18 22:57:08 +0100

    p977 solution

--- problems/p977.h
+++ problems/p977.h
@@ -1,8 +1,53 @@
 #pragma once
+#include <vector>
 
 namespace p977
 {
     class Solution
     {
+      public:
+        std::vector<int> sortedSquares(std::vector<int>& nums)
+        {
+            size_t N = nums.size();
+
+            size_t indexGZ = 0;
+            while (indexGZ < N && nums[indexGZ] < 0)
+            {
+                nums[indexGZ] *= nums[indexGZ];
+                ++indexGZ;
+            }
+
+            for (size_t i = indexGZ; i < N; ++i)
+                nums[i] *= nums[i];
+
+            std::vector<int> result(N, 0);
+            size_t i = 0;
+            size_t currN = indexGZ;
+            size_t currP = indexGZ;
+
+            while (currN > 0 && currP < N)
+            {
+                if (nums[currN - 1] > nums[currP])
+                {
+                    result[i++] = nums[currP];
+                    ++currP;
+                }
+                else
+                {
+                    result[i++] = nums[currN - 1];
+                    --currN;
+                }
+            }
+
+            while (currN > 0)
+            {
+                result[i++] = nums[currN - 1];
+                --currN;
+            }
+            while (currP < N)
+                result[i++] = nums[currP++];
+
+            return result;
+        }
     };
 }
--- tests/p977.cpp
+++ tests/p977.cpp
@@ -4,5 +4,11 @@
 
 TEST(p977, t0)
 {
-    [[maybe_unused]] p977::Solution s;
+    p977::Solution s;
+
+    std::vector<int> nums = {-4, -1, 0, 3, 10};
+    auto res = s.sortedSquares(nums);
+
+    std::vector<int> expected = {0, 1, 9, 16, 100};
+    EXPECT_EQ(res, expected);
 }
