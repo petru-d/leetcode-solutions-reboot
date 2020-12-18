9feab27c48f0208898c76619cdc434b222c65309
Petru-Nicu Dumitrache <berserk.ro@gmail.com>
2020-12-18 22:39:52 +0100

    p163 solution

--- problems/p163.h
+++ problems/p163.h
@@ -1,8 +1,47 @@
 #pragma once
+#include <vector>
+#include <string>
 
 namespace p163
 {
     class Solution
     {
+      public:
+        std::vector<std::string> findMissingRanges(std::vector<int>& nums, int lower, int upper)
+        {
+            std::vector<std::string> res;
+            auto add_range = [&res](int l, int r) {
+                if (l == r)
+                    return;
+
+                if (l + 1 == r)
+                {
+                    res.push_back(std::to_string(l));
+                    return;
+                }
+
+                std::string range_str = std::to_string(l);
+                range_str += "->";
+                range_str += std::to_string(r - 1);
+
+                res.push_back(std::move(range_str));
+            };
+
+            if (nums.empty())
+            {
+                add_range(lower, upper + 1);
+                return res;
+            }
+
+            auto start = lower;
+            for (const auto n : nums)
+            {
+                add_range(start, n);
+                start = n + 1;
+            }
+            add_range(nums.back() + 1, upper + 1);
+
+            return res;
+        }
     };
 }
--- tests/p163.cpp
+++ tests/p163.cpp
@@ -4,5 +4,12 @@
 
 TEST(p163, t0)
 {
-    [[maybe_unused]] p163::Solution s;
+    p163::Solution s;
+    std::vector<int> nums = {0, 1, 3, 50, 75};
+
+    auto res = s.findMissingRanges(nums, 0, 99);
+
+    std::vector<std::string> expected = {"2", "4->49", "51->74", "76->99"};
+    EXPECT_EQ(res, expected);
+    
 }
