4b55bda494b69ac8a7f9677aa4cb301c3050cca6
Petru-Nicu Dumitrache <berserk.ro@gmail.com>
2020-12-18 22:44:33 +0100

    p173

--- problems/p173.h
+++ problems/p173.h
@@ -1,8 +1,41 @@
 #pragma once
+#include "util_binary_tree.h"
+#include <stack>
 
 namespace p173
 {
-    class Solution
+    class BSTIterator
     {
+      public:
+        BSTIterator(TreeNode* root)
+        {
+            pushLeft(root);
+        }
+
+        int next()
+        {
+            auto* top = _nodes.top();
+            _nodes.pop();
+            pushLeft(top->right);
+
+            return top->val;
+        }
+
+        bool hasNext()
+        {
+            return !_nodes.empty();
+        }
+
+      private:
+        void pushLeft(TreeNode* root)
+        {
+            while (root)
+            {
+                _nodes.push(root);
+                root = root->left;
+            }
+        }
+
+        std::stack<TreeNode*> _nodes;
     };
 }
--- tests/p173.cpp
+++ tests/p173.cpp
@@ -4,5 +4,9 @@
 
 TEST(p173, t0)
 {
-    [[maybe_unused]] p173::Solution s;
+    auto root = deserialize_tree("7(3,15(9,20))");
+    p173::BSTIterator iter(root);
+
+    auto res = iter.next();
+    EXPECT_EQ(res, 3);
 }
