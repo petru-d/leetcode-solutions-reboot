347756f52530112159f5151d4ccb4fa1d597df04
Petru-Nicu Dumitrache <berserk.ro@gmail.com>
2021-02-13 23:10:24 +0100

    p1640 solution

--- problems/p1640.h
+++ problems/p1640.h
@@ -1,8 +1,33 @@
 #pragma once
+#include <vector>
+#include <unordered_map>
 
 namespace p1640
 {
     class Solution
     {
+      public:
+        bool canFormArray(std::vector<int>& arr, std::vector<std::vector<int>>& pieces)
+        {
+            std::unordered_map<int, size_t> h;
+            auto A = arr.size();
+            for (size_t i = 0; i < A; ++i)
+                h[arr[i]] = i;
+
+            for (const auto& p : pieces)
+            {
+                auto startPos = h.find(p[0]);
+                if (startPos == h.end())
+                    return false;
+                auto P = p.size();
+                for (size_t i = 1; i < P; ++i)
+                {
+                    if (i + startPos->second >= A || arr[i + startPos->second] != p[i])
+                        return false;
+                }
+            }
+
+            return true;
+        }
     };
 }
--- tests/p1640.cpp
+++ tests/p1640.cpp
@@ -4,5 +4,12 @@
 
 TEST(p1640, t0)
 {
-    [[maybe_unused]] p1640::Solution s;
+     p1640::Solution s;
+
+     std::vector<int> arr = {15, 88};
+     std::vector<std::vector<int>> pieces = {{88}, {15}};
+
+     auto res= s.canFormArray(arr, pieces);
+
+     EXPECT_TRUE(res);
 }
