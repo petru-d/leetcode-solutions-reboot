734c44ea48fbda84132096724b0ea81b76a29d94
Petru-Nicu Dumitrache <berserk.ro@gmail.com>
2021-02-13 22:19:11 +0100

    p1700 solution

--- problems/p1700.h
+++ problems/p1700.h
@@ -1,8 +1,37 @@
 #pragma once
+#include <vector>
 
 namespace p1700
 {
     class Solution
     {
+      public:
+        int countStudents(std::vector<int>& students, std::vector<int>& sandwiches)
+        {
+            // students can be viewed as a homogenous blob
+            size_t zero_students = 0;
+            size_t one_students = 0;
+            for (const auto s : students)
+                s == 0 ? ++zero_students : ++one_students;
+
+            auto S = sandwiches.size();
+            for (size_t i = 0; i < S; ++i)
+            {
+                if (sandwiches[i] == 0)
+                {
+                    if (zero_students == 0)
+                        return int(S - i);
+                    --zero_students;
+                }
+                else
+                {
+                    if (one_students == 0)
+                        return int(S - i);
+                    --one_students;
+                }
+            }
+
+            return 0;
+        }
     };
 }
--- tests/p1700.cpp
+++ tests/p1700.cpp
@@ -4,5 +4,12 @@
 
 TEST(p1700, t0)
 {
-    [[maybe_unused]] p1700::Solution s;
+     p1700::Solution s;
+
+     std::vector<int> students = {1, 1, 1, 0, 0, 1};
+     std::vector<int> sandwiches = {1, 0, 0, 0, 1, 1};
+
+     auto res = s.countStudents(students, sandwiches);
+
+     EXPECT_EQ(res, 3);
 }
