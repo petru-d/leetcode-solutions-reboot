ab4139be4bd4e588f3f9f031d4c8b78d0db0c8f0
Petru-Nicu Dumitrache <berserk.ro@gmail.com>
2021-02-14 22:33:23 +0100

    p339 solution

--- problems/p339.h
+++ problems/p339.h
@@ -1,8 +1,83 @@
 #pragma once
+#include <vector>
 
 namespace p339
 {
+    // This is the interface that allows for creating nested lists.
+    // You should not implement it, or speculate about its implementation
+    class NestedInteger
+    {
+      public:
+        // Constructor initializes an empty nested list.
+        NestedInteger() = default;
+
+        // Constructor initializes a single integer.
+        NestedInteger(int value)
+        {
+            _integer = true;
+            _val = value;
+        }
+
+        // Return true if this NestedInteger holds a single integer, rather than a nested list.
+        bool isInteger() const
+        {
+            return _integer;
+        }
+
+        // Return the single integer that this NestedInteger holds, if it holds a single integer
+        // The result is undefined if this NestedInteger holds a nested list
+        int getInteger() const
+        {
+            return _val;
+        }
+
+        // Set this NestedInteger to hold a single integer.
+        void setInteger(int value)
+        {
+            _integer = true;
+            _val = value;
+        }
+
+        // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+        void add(const NestedInteger& ni)
+        {
+            _integer = false;
+            _list.push_back(ni);
+        }
+
+        // Return the nested list that this NestedInteger holds, if it holds a nested list
+        // The result is undefined if this NestedInteger holds a single integer
+        const std::vector<NestedInteger>& getList() const
+        {
+            return _list;
+        }
+
+      private:
+        std::vector<NestedInteger> _list;
+        int _val = 0;
+        bool _integer = false;
+    };
+
     class Solution
     {
+      public:
+        int depthSum(std::vector<NestedInteger>& nestedList)
+        {
+            return depthSumC(nestedList, 1);
+        }
+
+        int depthSumC(const std::vector<NestedInteger>& nestedList, int depth)
+        {
+            int result = 0;
+            for (const auto& ni : nestedList)
+            {
+                if (ni.isInteger())
+                    result += depth * ni.getInteger();
+                else
+                    result += depthSumC(ni.getList(), depth + 1);
+            }
+
+            return result;
+        }
     };
 }
--- tests/p339.cpp
+++ tests/p339.cpp
@@ -4,5 +4,21 @@
 
 TEST(p339, t0)
 {
-    [[maybe_unused]] p339::Solution s;
+    p339::Solution s;
+
+    p339::NestedInteger first;
+    first.add(p339::NestedInteger(1));
+    first.add(p339::NestedInteger(1));
+
+    p339::NestedInteger second(2);
+
+    p339::NestedInteger third;
+    third.add(p339::NestedInteger(1));
+    third.add(p339::NestedInteger(1));
+
+    std::vector<p339::NestedInteger> l = {first, second, third};
+
+    auto res = s.depthSum(l);
+
+    EXPECT_EQ(res, 10);
 }
