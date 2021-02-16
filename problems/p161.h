ccee06854f433205277ca76dab4185aa42147fe5
Petru-Nicu Dumitrache <berserk.ro@gmail.com>
2021-02-16 21:24:02 +0100

    p161 solution

--- problems/p161.h
+++ problems/p161.h
@@ -1,8 +1,58 @@
 #pragma once
+#include <string>
 
 namespace p161
 {
     class Solution
     {
+      public:
+        bool isOneEditDistance(std::string s, std::string t)
+        {
+            if (s.size() > t.size())
+                std::swap(s, t);
+
+            if (t.size() - s.size() >= 2)
+                return false;
+
+            int currCost = 0;
+
+            int currS = 0;
+            int currT = 0;
+            while (currT < t.size())
+            {
+                if (currS == s.size())
+                {
+                    ++currCost;
+                    if (currCost > 1)
+                        return false;
+                    ++currT;
+                    continue;
+                }
+
+                if (s[currS] == t[currT])
+                {
+                    ++currS;
+                    ++currT;
+                }
+                else
+                {
+                    ++currCost;
+                    if (currCost > 1)
+                        return false;
+
+                    if (s.size() == t.size())
+                    {
+                        ++currS;
+                        ++currT;
+                    }
+                    else
+                    {
+                        ++currT;
+                    }
+                }
+            }
+
+            return currCost == 1;
+        }
     };
 }
--- tests/p161.cpp
+++ tests/p161.cpp
@@ -4,5 +4,8 @@
 
 TEST(p161, t0)
 {
-    [[maybe_unused]] p161::Solution s;
+    p161::Solution s;
+
+    auto res = s.isOneEditDistance("A", "B");
+    EXPECT_TRUE(res);
 }
