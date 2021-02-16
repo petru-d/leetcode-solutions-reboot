1133ff3dc8c9b450c28451199407d3df2b3db299
Petru-Nicu Dumitrache <berserk.ro@gmail.com>
2021-02-16 21:49:10 +0100

    p750 solution

--- problems/p750.h
+++ problems/p750.h
@@ -1,8 +1,60 @@
 #pragma once
+#include <vector>
 
 namespace p750
 {
     class Solution
     {
+      public:
+        int countCornerRectangles(std::vector<std::vector<int>>& grid)
+        {
+            auto R = grid.size();
+            auto C = grid.front().size();
+
+            std::vector<std::vector<size_t>> col_ones(C, std::vector<size_t>{});
+
+            for (size_t i = 0; i < C; ++i)
+                for (size_t j = 0; j < R; ++j)
+                {
+                    if (grid[j][i] == 1)
+                        col_ones[i].push_back(j);
+                }
+
+            int res = 0;
+            for (size_t f = 0; f < C; ++f)
+                for (size_t s = f + 1; s < C; ++s)
+                {
+                    int eq_points = get_eq_points(col_ones[f], col_ones[s]);
+                    if (eq_points >= 2)
+                        res += eq_points * (eq_points - 1) / 2;
+                }
+
+            return res;
+        }
+
+        int get_eq_points(const std::vector<size_t>& f, const std::vector<size_t>& s)
+        {
+            int eq_points = 0;
+            size_t pos_f = 0;
+            size_t pos_s = 0;
+            while (pos_f < f.size() && pos_s < s.size())
+            {
+                if (f[pos_f] == s[pos_s])
+                {
+                    ++pos_s;
+                    ++eq_points;
+                }
+                else if (f[pos_f] < s[pos_s])
+                {
+                    ++pos_f;
+                }
+                else
+                {
+                    ++pos_s;
+                }
+            }
+
+            return eq_points;
+        }
     };
 }
--- tests/p750.cpp
+++ tests/p750.cpp
@@ -4,5 +4,15 @@
 
 TEST(p750, t0)
 {
-    [[maybe_unused]] p750::Solution s;
+     p750::Solution s;
+
+     std::vector<std::vector<int>> grid = {
+         {1, 1, 1},
+         {1, 1, 1},
+         {1, 1, 1}
+     };
+
+     auto res= s.countCornerRectangles(grid);
+
+     EXPECT_EQ(res, 9);
 }
