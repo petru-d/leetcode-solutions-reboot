01cff57dc928add7789d662d298e7f7579254cfa
Petru-Nicu Dumitrache <berserk.ro@gmail.com>
2021-02-16 21:30:20 +0100

    1329 solution

--- problems/p1329.h
+++ problems/p1329.h
@@ -1,8 +1,47 @@
 #pragma once
+#include <vector>
+#include <algorithm>
 
 namespace p1329
 {
     class Solution
     {
+      public:
+        std::vector<std::vector<int>> diagonalSort(std::vector<std::vector<int>>& mat)
+        {
+            size_t L = mat.size();
+            size_t C = mat.front().size();
+
+            auto sd = [&](size_t x, size_t y) {
+                auto oldx = x;
+                auto oldy = y;
+                std::vector<int> v;
+                v.reserve(std::max(L, C));
+                while (x < L && y < C)
+                {
+                    v.push_back(mat[x][y]);
+                    ++x;
+                    ++y;
+                }
+
+                std::sort(v.begin(), v.end());
+
+                auto V = v.size();
+                for (size_t i = 0; i < V; ++i)
+                {
+                    mat[oldx][oldy] = v[i];
+                    ++oldx;
+                    ++oldy;
+                }
+            };
+
+            for (size_t i = 0; i < C; ++i)
+                sd(0, i);
+
+            for (size_t i = 1; i < L; ++i)
+                sd(i, 0);
+
+            return std::move(mat);
+        }
     };
 }
--- tests/p1329.cpp
+++ tests/p1329.cpp
@@ -4,5 +4,11 @@
 
 TEST(p1329, t0)
 {
-    [[maybe_unused]] p1329::Solution s;
+    p1329::Solution s;
+
+    std::vector<std::vector<int>> mat = {{2, 1}, {1, 0}};
+
+    mat = s.diagonalSort(mat);
+
+    EXPECT_EQ(mat, std::vector<std::vector<int>>({{0, 1}, {1, 2}}));
 }
