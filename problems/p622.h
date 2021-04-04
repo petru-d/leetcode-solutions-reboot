4af992e255969bb04ccd5894a53310f635189067
petrud <berserk.ro@gmail.com>
2021-04-04 22:01:56 +0200

    p622 solution

--- problems/p622.h
+++ problems/p622.h
@@ -1,8 +1,69 @@
 #pragma once
+#include <vector>
 
 namespace p622
 {
-    class Solution
+    class MyCircularQueue
     {
+      public:
+        MyCircularQueue(int k) : _buffer(k, 0), _head(0), _tail(0), _full(false)
+        {
+        }
+
+        bool enQueue(int value)
+        {
+            if (isFull())
+                return false;
+
+            _buffer[_tail] = value;
+            _tail = (_tail + 1) % _buffer.size();
+
+            if (_tail == _head)
+                _full = true;
+
+            return true;
+        }
+
+        bool deQueue()
+        {
+            if (isEmpty())
+                return false;
+
+            _head = (_head + 1) % _buffer.size();
+
+            _full = false;
+
+            return true;
+        }
+
+        int Front()
+        {
+            if (isEmpty())
+                return -1;
+            return _buffer[_head];
+        }
+
+        int Rear()
+        {
+            if (isEmpty())
+                return -1;
+
+            return _tail == 0 ? _buffer.back() : _buffer[_tail - 1];
+        }
+
+        bool isEmpty()
+        {
+            return _head == _tail && !_full;
+        }
+
+        bool isFull()
+        {
+            return _full;
+        }
+
+        std::vector<int> _buffer;
+        size_t _head = -1;
+        size_t _tail = -1;
+        bool _full = false;
     };
 }
--- tests/p622.cpp
+++ tests/p622.cpp
@@ -4,5 +4,16 @@
 
 TEST(p622, t0)
 {
-    [[maybe_unused]] p622::Solution s;
+    p622::MyCircularQueue qq(3);
+
+    auto res = qq.enQueue(1);
+    EXPECT_TRUE(res);
+    res = qq.enQueue(2);
+    EXPECT_TRUE(res);
+    res = qq.enQueue(3);
+    EXPECT_TRUE(res);
+    res = qq.enQueue(4);
+    EXPECT_FALSE(res);
+    auto r = qq.Rear();
+    EXPECT_EQ(r, 3);
 }
